dc609d048e509070f5711cf38ea7a214619bb898
ratlifffamilynt-arch <ratlifffamilynt@gmail.com>
2025-08-20 20:07:09 -0500

    Update snake.js

--- snake.js
+++ snake.js
@@ -23,7 +23,7 @@ function initGame() {
   restartBtn.style.display = 'none';
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   if (game) clearInterval(game);
-  game = setInterval(gameLoop, 2000);
+  game = setInterval(gameLoop, 1000);
   canvas.focus();
 }
 
